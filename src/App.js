4ce551e394961dc242fd9790defe8ce58d7b2bac
tjgillweb <tjgillweb@gmail.com>
2020-05-15 22:18:22 +0530

    Add name input, use two way binding

--- src/App.js
+++ src/App.js
@@ -25,6 +25,16 @@ class App extends Component {
       //otherState: personsState.otherState
     });
   }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Max', age:28},
+        {name: event.target.value, age:29},
+        {name: 'Stephanie', age:27}
+      ]
+    })
+  }
   render(){
     return (
         <div className="App">
@@ -37,7 +47,8 @@ class App extends Component {
         <Person 
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!')} >My Hobbies: Racing</Person>
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
         <Person 
           name={this.state.persons[2].name} 
           age={this.state.persons[2].age} />
