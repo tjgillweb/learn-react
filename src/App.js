3e813d1d5c8cda98eafdf310dc9f1e3be5c87065
tjgillweb <tjgillweb@gmail.com>
2020-05-18 21:17:53 +0530

    Add dynamic content using if

--- src/App.js
+++ src/App.js
@@ -76,55 +76,34 @@ class App extends Component {
       padding: '10px 20px',
       cursor: 'pointer'
     };
-    return ( <
-      div className = "App" >
-      <
-      h1 > Hi, I 'm a React App</h1>  <
-      p > This is really working! < /p>  <
-      button style = {
-        style
-      }
-      onClick = {
-        this.togglePersonsHandler
-      } > Toggle Persons < /button>
-      {
-        this.state.showPersons ?
+
+    let persons = null;
+    if(this.state.showPersons){
+      persons = (
         <div>
-      <
-      Person name = {
-        this.state.persons[0].name
-      }
-      age = {
-        this.state.persons[0].age
-      }
-      />  <
-      Person name = {
-        this.state.persons[1].name
-      }
-      age = {
-        this.state.persons[1].age
-      }
-      click = {
-        this.switchNameHandler.bind(this, 'Max!')
-      }
-      changed = {
-        this.nameChangedHandler
-      } > My Hobbies: Racing < /Person>  <
-      Person name = {
-        this.state.persons[2].name
-      }
-      age = {
-        this.state.persons[2].age
-      }
-      /> 
-      </div> : null
-        }
-          <
-      /div>
+          <Person name = {this.state.persons[0].name}
+                  age = {this.state.persons[0].age}/>
+          <Person name = {this.state.persons[1].name}
+                  age = {this.state.persons[1].age}
+                  click = {this.switchNameHandler.bind(this, 'Max!')}
+                  changed = {this.nameChangedHandler} > My Hobbies: Racing 
+          </Person>  
+          <Person name = {this.state.persons[2].name}
+                  age = {this.state.persons[2].age}/> 
+        </div>
+      )
+    }
+    return ( 
+      <div className = "App" >
+      <h1> Hi, I 'm a React App</h1>  
+      <p> This is really working! </p>  
+      <button style = {style}
+              onClick = {this.togglePersonsHandler} > Toggle Persons 
+      </button>
+      {persons}
+      </div>
     );
-
   }
-
 }
 
 export default App;
