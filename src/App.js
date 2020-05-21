3b4d6abd4246ddaed8c9770485d9724ee1f65b41
tjgillweb <tjgillweb@gmail.com>
2020-05-21 18:12:38 +0530

    Add StyledDiv to Person using Styled Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {
   Component
 } from 'react';
-import Radium, { StyleRoot } from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -109,18 +108,15 @@ class App extends Component {
     }
 
     return ( 
-      <StyleRoot>
         <div className = "App" >
         <h1> Hi, I 'm a React App</h1>  
         <p className={classes.join(' ')}> This is really working! </p>  
-        <button style = {myStyle}
-                onClick = {this.togglePersonsHandler} > Toggle Persons 
+        <button style={myStyle} onClick = {this.togglePersonsHandler} > Toggle Persons 
         </button>
         {persons}
         </div>
-      </StyleRoot>
     );
   }
 }
 
-export default Radium(App);
+export default App;
