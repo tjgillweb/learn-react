d9b4e8da1ca6ace2db2ee2365787a93a24fe94ee
tjgillweb <tjgillweb@gmail.com>
2020-05-22 21:04:03 +0530

    Add CSS modules to Person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,33 +1,16 @@
-import React from 'react';
-import styled from 'styled-components';	
-import './Person.css';
+import React from 'react'; 
+import classes from './Person.module.css';
 
-const StyledDiv = styled.div`
-  background: #fefefe;
-  border: 1px solid #eee;
-  border-radius: 10px;
-  box-shadow: 0 5px 10px rgba(188, 177, 178, 0.6);
-  height: auto;
-  padding: 20px;
-  text-align: center;
-  display: block;
-  margin: 20px auto;
-
-  @media (min-width: 500px) {
-    width: 450px;
-  }
-`;
 
 const person = props => {
   return (
-    // <div className="Person" style={style}>
-    <StyledDiv>
+    <div className={classes.Person}>
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </StyledDiv>
+    </div>
   );
 };
 
