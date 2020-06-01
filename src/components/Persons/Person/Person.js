64f27a833791849102270145ddc28d2718889322
tjgillweb <tjgillweb@gmail.com>
2020-06-01 19:31:50 +0530

    Render adjacent JSX elements using wrapper Aux component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react'; 
+import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.module.css';
 
 class Person extends Component{
   render(){
     console.log('[Person.js] rendering...');
     return (
-      <div className={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>
+      </Aux>
     );
   }
 }
