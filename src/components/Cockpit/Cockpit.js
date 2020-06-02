8f6c69f92242bf31c392a839402eb3d1c4b01dfb
tjgillweb <tjgillweb@gmail.com>
2020-06-02 23:37:52 +0530

    Use Ref with React Hooks

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,17 @@
-import React, {useEffect} from 'react'; 
+import React, {useEffect, useRef} from 'react'; 
 import classes from './Cockpit.module.css';
 
 const Cockpit = props => {
+    const toggleBtnRef = useRef(null);
+    
+
     useEffect(() => {
       console.log('[Cockpit.js] useEffect');
       //Http request...
-      setTimeout(() => {
-        alert('Saved data to cloud');
-      }, 1000);
+      // setTimeout(() => {
+      //   alert('Saved data to cloud');
+      // }, 1000);
+      toggleBtnRef.current.click();
       return () => {
         console.log('[Cockpit.js] cleanup work in useEffect');
       };
@@ -37,7 +41,7 @@ const Cockpit = props => {
         <div className={classes.Cockpit}>
         <h1>{props.title}</h1>  
         <p className={assignedClasses.join(' ')}> This is really working! </p>  
-        <button className={btnClass.join(' ')} onClick = {props.clicked} > Toggle Persons 
+        <button ref={toggleBtnRef } className={btnClass.join(' ')} onClick = {props.clicked} > Toggle Persons 
         </button>
         </div>
     )
